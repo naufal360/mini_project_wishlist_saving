ee7d3e5065600cd94a0b281298156c9544c9f55c
naufal360 <naufal360.an@gmail.com>
2022-11-03 22:59:12 +0700

    add jwt log trailingslash middleware

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,1 +1,19 @@
 package middleware
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func CreateToken(userId string, username string) (string, error) {
+	key := []byte(os.Getenv("SECRET_JWT_KEY"))
+	claims := jwt.MapClaims{}
+	claims["userId"] = userId
+	claims["username"] = username
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(key)
+}
--- middleware/trailing_slash.go
+++ middleware/trailing_slash.go
@@ -1,1 +1,10 @@
 package middleware
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func RemoveSlash(routes *echo.Echo) {
+	routes.Pre(middleware.RemoveTrailingSlash())
+}
