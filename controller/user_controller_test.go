6bd229db8b8f848466e3dea4ed6ae4de930e1516
naufal360 <naufal360.an@gmail.com>
2022-11-06 15:52:15 +0700

    correction mock and wishlist

---
+++ controller/user_controller_test.go
@@ -0,0 +1,103 @@
+package controller
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"wishlist/dto/payload"
+	"wishlist/service/user/mocks"
+
+	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRegisterUser(t *testing.T) {
+	userTest := payload.Register{
+		Name:     "Ahmad Naufal",
+		Username: "naufal",
+		Email:    "naufal@mail.com",
+		Password: "user123naufal",
+	}
+
+	testCases := []struct {
+		name       string
+		body       payload.Register
+		buildStubs func(user_service *mocks.MockUserService)
+		checkBody  func(recorder *httptest.ResponseRecorder)
+	}{
+		{
+			"created",
+			userTest,
+			func(user_service *mocks.MockUserService) {
+				// hashingUser, _ := util.HashPassword(userTest.Password)
+				// arg := model.User{
+				// 	Name:     userTest.Name,
+				// 	Username: userTest.Username,
+				// 	Email:    userTest.Email,
+				// 	Password: hashingUser,
+				// }
+				user_service.EXPECT().
+					RegisterUser(gomock.Any()).
+					Times(1).
+					Return(nil)
+			},
+			func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				// requireBodyMatchUser(t, recorder.Body, userTest)
+			},
+		},
+	}
+
+	for _, v := range testCases {
+		t.Run(v.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			user_service := mocks.NewMockUserService(ctrl)
+			v.buildStubs(user_service)
+
+			// server := newTestServer(t, user_service)
+			recorder := httptest.NewRecorder()
+
+			data, err := json.Marshal(v.body)
+			require.NoError(t, err)
+
+			url := "/signup"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			request.Header.Set(echo.HeaderContentType, "application/json")
+
+			// server.router.ServeHTTP(recorder, request)
+			v.checkBody(recorder)
+		})
+	}
+}
+
+// func TestLoginUser(t *testing.T) {
+// 	user := payload.Login{
+// 		Username: "naufal",
+// 		Password: "",
+// 	}
+// }
+
+// func requireBodyMatchUser(t *testing.T, body *bytes.Buffer, user payload.Register) {
+// 	data, err := io.ReadAll(body)
+// 	require.NoError(t, err)
+
+// 	var gotUser model.User
+// 	err = json.Unmarshal(data, &gotUser)
+
+// 	require.NoError(t, err)
+// 	hashingUser, err := util.HashPassword(user.Password)
+
+// 	require.NoError(t, err)
+
+// 	require.Equal(t, user.Name, gotUser.Name)
+// 	require.Equal(t, user.Username, gotUser.Username)
+// 	require.Equal(t, user.Email, gotUser.Email)
+// 	require.Equal(t, hashingUser, gotUser.Password)
+// }
--- go.mod
+++ go.mod
@@ -9,12 +9,14 @@ require (
 	github.com/joho/godotenv v1.4.0
 	github.com/labstack/echo/v4 v4.9.1
 	github.com/sirupsen/logrus v1.9.0
+	github.com/stretchr/testify v1.8.0
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
 	gorm.io/driver/mysql v1.4.3
 	gorm.io/gorm v1.24.1-0.20221019064659-5dd2bb482755
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
@@ -22,13 +24,14 @@ require (
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/stretchr/testify v1.8.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
