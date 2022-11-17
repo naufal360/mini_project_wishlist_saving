555e1b182462a610b9aa6f84f8f943c09d176c45
Ahmad Naufal Fadhil <72871844+naufal360@users.noreply.github.com>
2022-11-17 21:01:10 +0700

    Development (#6)
    * remove unused user endpoint, plural the wishlists route
    
    * fix the ambigous of payload parameter to payloads
    
    * renew README
    
    * redeploy

--- route/wishlist/wishlist_routes.go
+++ route/wishlist/wishlist_routes.go
@@ -10,15 +10,16 @@ import (
 
 func WishlistAuthenticated(routes *echo.Echo, api *controller.WishlistController) {
 	authUser := routes.Group("/v1")
+	
 	authUser.Use(middleware.JWT([]byte(os.Getenv("SECRET_JWT_KEY"))))
 
 	{
-		authUser.GET("/wishlist", api.ReadWishlist)
-		authUser.GET("/wishlist/:wishlistid", api.ReadWishlistById)
-		authUser.GET("/wishlist/:wishlistid/recommend", api.ReadRecommend)
-		authUser.POST("/wishlist", api.CreateWishlist)
-		authUser.PUT("/wishlist/:wishlistid", api.UpdateWishlist)
-		authUser.PUT("/wishlist/:wishlistid/balance", api.UpdateBalance)
-		authUser.DELETE("/wishlist/:wishlistid", api.DeleteWishlist)
+		authUser.GET("/wishlists", api.ReadWishlist)
+		authUser.GET("/wishlists/:wishlistid", api.ReadWishlistById)
+		authUser.GET("/wishlists/:wishlistid/recommend", api.ReadRecommend)
+		authUser.POST("/wishlists", api.CreateWishlist)
+		authUser.PUT("/wishlists/:wishlistid", api.UpdateWishlist)
+		authUser.PUT("/wishlists/:wishlistid/balances", api.UpdateBalance)
+		authUser.DELETE("/wishlists/:wishlistid", api.DeleteWishlist)
 	}
 }
